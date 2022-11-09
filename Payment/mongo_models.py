459bade0f5ab178e0c20fd561fdfb167e743c36c
Hossein <you@example.com>
2022-11-09 18:31:34 +0330

    verify_payment method in Payment.views has been added

--- Payment/mongo_models.py
+++ Payment/mongo_models.py
@@ -5,7 +5,7 @@ import datetime
 class Factor(Document):
     order_id = ObjectIdField(required=True, unique=True)
     amount = IntField(required=True)
-    status = StringField(max_length=10, required=True, default="created")  # created - paid
+    status = StringField(required=True, default="created")  # created - paid
     status_code = IntField(required=True, default=-1)
     created_at = DateTimeField(required=True, default=datetime.datetime.now)
 
@@ -13,8 +13,8 @@ class Factor(Document):
 class Payment(Document):
     amount = IntField(required=True)
     factor_id = ReferenceField(Factor)
-    trace_id = StringField(max_length=50, required=False)
-    status = StringField(max_length=10, required=True, default="not-paid")  # not-paid , succeeded , failed
+    trace_id = StringField(required=False)
+    status = StringField(required=True, default="not-paid")  # not-paid , succeeded , failed
     status_code = IntField(required=True, default=-1)  # default = -1
-    status_description = StringField(required=False)
+    description = StringField(required=False)
     created_at = DateTimeField(required=True, default=datetime.datetime.now)
--- Payment/views.py
+++ Payment/views.py
@@ -2,10 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
-
+import os
 import zibal.zibal as zibal
 from Payment.mongo_models import Factor, Payment
-import os
+
+merchant = os.getenv("ZIBAL_MERCHANT")
 
 
 @csrf_exempt
@@ -20,14 +21,13 @@ def pay_factor(req, factor_id):
         amount=factor.amount,
     ).save()
 
-    merchant = os.getenv("ZIBAL_MERCHANT")
     callback_url = f"http://127.0.0.1:8000/payment/verify_payment/{payment.id}"
 
     zb = zibal.zibal(merchant, callback_url)
     amount = payment.amount * 10
     request_to_zibal = zb.request(amount)
 
-    if request_to_zibal['result'] is not 100:
+    if request_to_zibal['result'] != 100:
         return HttpResponse(status=500)
 
     payment.trace_id = str(request_to_zibal['trackId'])
@@ -37,7 +37,29 @@ def pay_factor(req, factor_id):
 
 
 @csrf_exempt
-@require_http_methods(["POST"])
+@require_http_methods(["GET"])
 def verify_payment(request, payment_id):
-    print(payment_id)
-    return HttpResponse("verify_payment test")
+    payment = Payment.objects.get(
+        pk=payment_id
+    )
+
+    zb = zibal.zibal(merchant, None)
+
+    verify_zibal = zb.verify(payment.trace_id)
+    status = verify_zibal['result']
+
+    payment.status = verify_zibal['message']
+    payment.status_code = verify_zibal['result']
+    payment.save()
+
+    if status == 100 or status == 201:  # payment was successful and now factor's status must change to paid
+        factor = Factor.objects.get(
+            pk=payment.factor_id.id
+        )
+        factor.status = "paid"
+        factor.status_code = 1
+        factor.save()
+
+        # send message was successful to celery
+
+    return HttpResponse(payment.status)
