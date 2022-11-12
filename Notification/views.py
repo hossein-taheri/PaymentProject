dce7e1b1517f2f74ff9aa7f5046ef847547ecb62
Hossein <you@example.com>
2022-11-12 13:37:50 +0330

    Kavenegar has been replaced with RayganSMS

--- Notification/views.py
+++ Notification/views.py
@@ -3,7 +3,24 @@ import os
 from django.shortcuts import render
 from kavenegar import *
 
-kavenegar_api_key = os.getenv("KAVENEGAR_API_KEY")
-print(kavenegar_api_key)
+user_name = os.getenv('RAYGANSMS_USERNAME')
+password = os.getenv('RAYGANSMS_PASSWORD')
+sender_number = os.getenv('RAYGANSMS_SENDER_NUMBER')
 
 
+def SendSMS(receiver, message):
+    import requests
+
+    url = "https://RayganSMS.com/SendMessageWithUrl.ashx"
+
+    params = {
+        "UserName": user_name,
+        "Password": password,
+        "PhoneNumber": sender_number,
+        "MessageBody": message,
+        "RecNumber": receiver,
+    }
+
+    response = requests.request("GET", url, params=params)
+
+    print(response.text)
